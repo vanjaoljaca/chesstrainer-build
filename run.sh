bb78d2a020b8ba303a3182c326df50b9530bbbb8
Vanja Oljaca <vanja.oljaca@oracle.com>
2022-07-11 00:22:47 -0700

    new build

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cd ../chessapp && yarn build && cd ../chesstrainer-build && cp -r ../chessapp/build/* . && git add --all && git commit -m 'new build' && git push origin
+cd ../chessapp && yarn build && cd ../chesstrainer-build && cp -r ../chessapp/build/* ./Application && git add --all && git commit -m 'new build' && git push origin
